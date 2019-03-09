166495cb05d704fe24819d0aa8802554c528d50c
hegazy360 <m.hegazy94@hotmail.com>
2019-03-09 13:14:46 +0100

    Added Gormmet and basic responsive skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,127 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 import Home from './pages/Home';
 import List from './pages/List';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  Layer,
+  ResponsiveContext
+} from 'grommet';
+import { FormClose, Notification } from 'grommet-icons';
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6'
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px'
+    }
+  }
+};
+
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation="medium"
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
 
 class App extends Component {
+  state = {
+    showSidebar: false
+  };
   render() {
+    const { showSidebar } = this.state;
+
     const App = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/list" component={List} />
         </Switch>
       </div>
-    )
+    );
     return (
-      <Switch>
-        <App/>
-      </Switch>
+      <Grommet theme={theme} full>
+        <ResponsiveContext.Consumer>
+          {size => (
+            <Box fill>
+              <AppBar>
+                <Heading level="3" margin="none">
+                  My App
+                </Heading>
+                <Button
+                  icon={<Notification />}
+                  onClick={() =>
+                    this.setState(prevState => ({
+                      showSidebar: !prevState.showSidebar
+                    }))
+                  }
+                />
+              </AppBar>
+              <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+                <Box flex align="center" justify="center">
+                  app body
+                </Box>
+                {!showSidebar || size !== 'small' ? (
+                  <Collapsible direction="horizontal" open={showSidebar}>
+                    <Box
+                      flex
+                      width="medium"
+                      background="light-2"
+                      elevation="small"
+                      align="center"
+                      justify="center"
+                    >
+                      sidebar
+                    </Box>
+                  </Collapsible>
+                ) : (
+                  <Layer>
+                    <Box
+                      background="light-2"
+                      tag="header"
+                      justify="end"
+                      align="center"
+                      direction="row"
+                    >
+                      <Button
+                        icon={<FormClose />}
+                        onClick={() => this.setState({ showSidebar: false })}
+                      />
+                    </Box>
+                    <Box
+                      fill
+                      background="light-2"
+                      align="center"
+                      justify="center"
+                    >
+                      sidebar
+                    </Box>
+                  </Layer>
+                )}
+              </Box>
+            </Box>
+          )}
+        </ResponsiveContext.Consumer>
+        <Switch>
+          <App />
+        </Switch>
+      </Grommet>
     );
   }
 }
