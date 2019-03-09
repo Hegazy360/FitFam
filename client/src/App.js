d6b232a208868110dd86ffec2ebed6cc79d0135b
hegazy360 <m.hegazy94@hotmail.com>
2019-03-09 23:23:22 +0100

    Added lottie animations + responsive homepage with Grommet

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import List from './pages/List';
+// import { Route, Switch } from 'react-router-dom';
 import {
   Box,
   Button,
@@ -9,14 +7,23 @@ import {
   Heading,
   Grommet,
   Layer,
-  ResponsiveContext
+  ResponsiveContext,
+  Image,
+  Paragraph,
+  Carousel,
+  Text
 } from 'grommet';
-import { FormClose, Notification } from 'grommet-icons';
+import Lottie from 'react-lottie';
+import { Close, Menu, Info } from 'grommet-icons';
+// import Home from './pages/Home';
+// import List from './pages/List';
+import homepage from './assets/images/homepage.jpg';
 
 const theme = {
   global: {
     colors: {
-      brand: '#228BE6'
+      brand: 'rgb(105, 188, 186)',
+      white: '#FFFFFF'
     },
     font: {
       family: 'Roboto',
@@ -32,7 +39,7 @@ const AppBar = props => (
     direction="row"
     align="center"
     justify="between"
-    background="brand"
+    background="white"
     pad={{ left: 'medium', right: 'small', vertical: 'small' }}
     elevation="medium"
     style={{ zIndex: '1' }}
@@ -44,17 +51,18 @@ class App extends Component {
   state = {
     showSidebar: false
   };
+
   render() {
     const { showSidebar } = this.state;
 
-    const App = () => (
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/list" component={List} />
-        </Switch>
-      </div>
-    );
+    // const App = () => (
+    //   <div>
+    //     <Switch>
+    //       <Route exact path="/" component={Home} />
+    //       <Route path="/list" component={List} />
+    //     </Switch>
+    //   </div>
+    // );
     return (
       <Grommet theme={theme} full>
         <ResponsiveContext.Consumer>
@@ -62,10 +70,10 @@ class App extends Component {
             <Box fill>
               <AppBar>
                 <Heading level="3" margin="none">
-                  My App
+                  Trainiac
                 </Heading>
                 <Button
-                  icon={<Notification />}
+                  icon={<Menu />}
                   onClick={() =>
                     this.setState(prevState => ({
                       showSidebar: !prevState.showSidebar
@@ -75,7 +83,354 @@ class App extends Component {
               </AppBar>
               <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
                 <Box flex align="center" justify="center">
-                  app body
+                  <Image
+                    fit="cover"
+                    style={{ width: '100%', minHeight: '200px' }}
+                    src={homepage}
+                  />
+                  <br />
+                  <Heading
+                    margin="none"
+                    level="2"
+                    textAlign="center"
+                    style={{ fontWeight: 500 }}
+                  >
+                    Get fit faster
+                  </Heading>
+                  <Heading
+                    margin="medium"
+                    level="3"
+                    textAlign="center"
+                    style={{ fontWeight: 500 }}
+                    size="small"
+                  >
+                    Get a coach to help you get to the next level
+                  </Heading>
+
+                  <Box
+                    flex="grow"
+                    direction="row"
+                    align="center"
+                    gap="small"
+                    pad="xsmall"
+                  >
+                    <Button label="Start now" onClick={() => {}} />
+                    <Button
+                      plain
+                      icon={<Info color="brand" />}
+                      label="Know more"
+                      onClick={() => {}}
+                    />
+                  </Box>
+                  <br />
+                  <br />
+
+                  <Box fill="horizontal">
+                    <Box flex="grow" background="brand" align="center">
+                      <Heading
+                        level="4"
+                        color="white"
+                        textAlign="center"
+                        size="small"
+                        margin="large"
+                        style={{ fontWeight: 500 }}
+                      >
+                        90% of People Quit After 3 Months of Hitting the Gym
+                      </Heading>
+                      <Lottie
+                        options={{
+                          loop: true,
+                          autoplay: true,
+                          animationData: require('./assets/animations/stopwatch.json')
+                        }}
+                        height={270}
+                      />
+                      <Paragraph
+                        margin="small"
+                        textAlign="center"
+                        color="white"
+                      >
+                        We help people all around the world not to quit, by
+                        following, motivating and helping them improve and learn
+                        everything about their body and working out.
+                        <br />
+                        <br />
+                      </Paragraph>
+                    </Box>
+                    <Box flex="grow" background="#8BC34A" align="center">
+                      <Heading
+                        level="4"
+                        color="white"
+                        textAlign="center"
+                        size="small"
+                        margin="large"
+                        style={{ fontWeight: 500 }}
+                      >
+                        We get it, it's not easy.
+                      </Heading>
+                      <Lottie
+                        options={{
+                          loop: true,
+                          autoplay: true,
+                          animationData: require('./assets/animations/biking.json')
+                        }}
+                        height={270}
+                      />
+                      <Paragraph
+                        margin="small"
+                        textAlign="center"
+                        color="white"
+                      >
+                        We were all once in the same position.
+                        <br />
+                        Confused by all the one-size-fits-all diet tips and
+                        workout plans.
+                        <br />
+                        We wish we had an affordable direct way to become fit
+                        faster.
+                        <br />
+                        <br />
+                      </Paragraph>
+                    </Box>
+                    <Box flex="grow" background="accent-4" align="center">
+                      <Heading
+                        level="4"
+                        color="white"
+                        textAlign="center"
+                        size="small"
+                        margin="large"
+                        style={{ fontWeight: 500 }}
+                      >
+                        We're flexible
+                      </Heading>
+                      <Lottie
+                        options={{
+                          loop: true,
+                          autoplay: true,
+                          animationData: require('./assets/animations/dance.json')
+                        }}
+                        height={270}
+                      />
+                      <Paragraph
+                        margin="small"
+                        textAlign="center"
+                        color="white"
+                      >
+                        Most of Trainiac's team are young athletes who have been
+                        training most of their lives.
+                        <br />
+                        We don't believe in complicating what should be
+                        enjoyable.
+                        <br />
+                        Multiple plans, easy cancelation and refunds.
+                        <br />
+                        <br />
+                      </Paragraph>
+                    </Box>
+                    <Box flex="grow" background="#4CAF50" align="center">
+                      <Heading
+                        level="4"
+                        color="white"
+                        textAlign="center"
+                        size="small"
+                        margin="large"
+                        style={{ fontWeight: 500 }}
+                      >
+                        We're affordable
+                      </Heading>
+                      <Lottie
+                        options={{
+                          loop: true,
+                          autoplay: true,
+                          animationData: require('./assets/animations/atm.json')
+                        }}
+                        height={270}
+                      />
+                      <Paragraph
+                        margin="small"
+                        textAlign="center"
+                        color="white"
+                      >
+                        We're changing the game.
+                        <br />
+                        <br />
+                        Multiple plans, starting at $20.
+                        <br />
+                        Customize everything based on your schedule, goals and
+                        motivation.
+                        <br />
+                        <br />
+                      </Paragraph>
+                    </Box>
+                    <Box flex="grow" background="#4CAF50" align="center">
+                      <Carousel fill>
+                        <Box
+                          style={{ padding: '20px 30px 40px' }}
+                          align="center"
+                          height="350px"
+                          background="accent-1"
+                          justify="between"
+                        >
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="xxlarge"
+                            style={{ fontWeight: 600 }}
+                          >
+                            Basic - $25
+                          </Text>
+                          <br />
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            Custom workout plan
+                          </Text>
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            1 Monthly follow-up by one of the coaches
+                          </Text>
+                          {/* <Attraction size="large" /> */}
+                          <br />
+                          <br />
+                          <Button
+                            primary
+                            color="white"
+                            label="Start now"
+                            onClick={() => {}}
+                          />
+                        </Box>
+                        <Box
+                          style={{ padding: '20px 20px 40px' }}
+                          align="center"
+                          height="350px"
+                          background="accent-2"
+                          justify="between"
+                        >
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="xxlarge"
+                            style={{ fontWeight: 600 }}
+                          >
+                            Pro - $40
+                          </Text>
+                          <br />
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            Custom workout plan
+                          </Text>
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            2 Monthly follow-ups by one of the coaches
+                          </Text>
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            Personal communication line with one of the coaches
+                            for any questions
+                          </Text>
+                          {/* <Attraction size="large" /> */}
+                          <br />
+                          <br />
+                          <Button
+                            primary
+                            color="white"
+                            label="Start now"
+                            onClick={() => {}}
+                          />
+                        </Box>
+                        <Box
+                          style={{ padding: '20px 20px 40px' }}
+                          align="center"
+                          height="350px"
+                          background="accent-3"
+                          justify="between"
+                        >
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="xxlarge"
+                            style={{ fontWeight: 600 }}
+                          >
+                            Pro+ - $60
+                          </Text>
+                          <br />
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            Custom workout plan
+                          </Text>
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            Weekly follow-up by one of the coaches
+                          </Text>
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            Personal communication line with one of the coaches
+                            for any questions
+                          </Text>
+                          <Text
+                            margin="none"
+                            color="white"
+                            textAlign="center"
+                            size="medium"
+                            style={{ fontWeight: 500 }}
+                          >
+                            Diet planning and nutrition following
+                          </Text>
+                          {/* <Attraction size="large" /> */}
+                          <br />
+                          <br />
+                          <Button
+                            primary
+                            color="white"
+                            label="Start now"
+                            onClick={() => {}}
+                          />
+                        </Box>
+                      </Carousel>
+                    </Box>
+                  </Box>
                 </Box>
                 {!showSidebar || size !== 'small' ? (
                   <Collapsible direction="horizontal" open={showSidebar}>
@@ -100,7 +455,7 @@ class App extends Component {
                       direction="row"
                     >
                       <Button
-                        icon={<FormClose />}
+                        icon={<Close />}
                         onClick={() => this.setState({ showSidebar: false })}
                       />
                     </Box>
@@ -110,7 +465,13 @@ class App extends Component {
                       align="center"
                       justify="center"
                     >
-                      sidebar
+                      Add plans
+                      <br />
+                      Add trainiac beasts counter
+                      <br />
+                      Add team pictures
+                      <br />
+                      Add result pictures
                     </Box>
                   </Layer>
                 )}
@@ -118,9 +479,6 @@ class App extends Component {
             </Box>
           )}
         </ResponsiveContext.Consumer>
-        <Switch>
-          <App />
-        </Switch>
       </Grommet>
     );
   }
--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class List extends Component {
   // Initialize the state
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       list: []
-    }
+    };
   }
 
   // Fetch the list on first mount
@@ -17,9 +17,9 @@ class List extends Component {
   // Retrieves the list of items from the Express app
   getList = () => {
     fetch('/api/getList')
-    .then(res => res.json())
-    .then(list => this.setState({ list }))
-  }
+      .then(res => res.json())
+      .then(list => this.setState({ list }));
+  };
 
   render() {
     const { list } = this.state;
@@ -27,24 +27,19 @@ class List extends Component {
     return (
       <div className="App">
         <h1>List of Items</h1>
-        {/* Check to see if any items are found*/}
+        {/* Check to see if any items are found */}
         {list.length ? (
           <div>
             {/* Render the list of items */}
-            {list.map((item) => {
-              return(
-                <div>
-                  {item}
-                </div>
-              );
+            {list.map(item => {
+              return <div>{item}</div>;
             })}
           </div>
         ) : (
           <div>
             <h2>No List Items Found</h2>
           </div>
-        )
-      }
+        )}
       </div>
     );
   }
